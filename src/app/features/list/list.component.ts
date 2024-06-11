3af964baac6f044b1a6340fd4b7000c83abd7b03
Angela Talyta <tatazinhagms2@gmail.com>
2024-06-11 20:41:07 -0300

    "Inicio projeto gerenciador de produtos"

---
+++ src/app/features/list/list.component.ts
@@ -0,0 +1,22 @@
+import { HttpClient } from '@angular/common/http';
+import { Component, inject } from '@angular/core';
+
+@Component({
+  selector: 'app-list',
+  standalone: true,
+  imports: [],
+  templateUrl: './list.component.html',
+  styleUrl: './list.component.scss'
+})
+export class ListComponent {
+
+  httpClient = inject(HttpClient);
+
+  products: any[] = [];
+
+  ngOnInit() {
+    this.httpClient.get<any>('/api/products').subscribe((products) => {
+      this.products = products
+    });
+  }
+}
