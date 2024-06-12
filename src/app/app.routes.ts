549c17d87886ae99985e85afbc4e0e0c7fed9f6e
Angela Talyta <tatazinhagms2@gmail.com>
2024-06-11 23:21:24 -0300

    Criando roteamento para criar produto

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,7 +1,21 @@
 import { Routes } from '@angular/router';
 import { ListComponent } from './features/list/list.component';
+import { CreateComponent } from './features/create/create.component';
 
-export const routes: Routes = [{
+export const routes: Routes = [
+{
   path: '',
-  component: ListComponent
-}];
+  component: ListComponent,
+},
+{
+  path: 'create-product',
+  loadComponent: () =>
+    import('./features/create/create.component').then(m => m.CreateComponent)
+},
+];
+
+
+
+
+
+
